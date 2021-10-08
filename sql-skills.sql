bf94ba1fd067cfd788df8a8776a5f8f948a09e16
josegarcia1519 <50972343+josegarcia1519@users.noreply.github.com>
2021-10-08 11:19:41 -0600

    update

---
+++ sql-skills.sql
@@ -0,0 +1,33 @@
+--INSERT INTO artist (name)
+--VALUES ('lennon'), 
+--('ringo'),
+--('paul'),
+--('malone'),
+--('sasha')
+--;
+
+--SELECT * FROM artist
+--ORDER BY name ASC LIMIT 5;
+
+--SELECT first_name, last_name FROM employee
+--WHERE city = 'Calgary';
+
+--SELECT employee_id, last_name from employee
+--WHERE last_name = 'Edwards';
+
+--SELECT first_name, last_name FROM employee
+--WHERE reports_to =2;
+
+--SELECT COUNT(*) FROM employee
+--WHERE city = 'Lethbridge';
+
+--SELECT COUNT(*) FROM invoice
+--WHERE billing_country = 'USA';
+
+--SELECT MAX(total) FROM invoice;
+--SELECT MIN(total) FROM invoice;
+--SELECT total FROM invoice
+--WHERE total > 5;
+--SELECT COUNT(*) FROM invoice
+--WHERE total < 5;
+--SELECT SUM(total) FROM invoice;
